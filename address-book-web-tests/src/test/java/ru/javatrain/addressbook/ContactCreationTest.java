f63829cc34e658dd5e06a8b0ba79253c22100095
PolinaRil <107141612+PolinaRil@users.noreply.github.com>
2022-06-28 19:52:15 +0300

    add parameter object to ContactCreationTest

--- address-book-web-tests/src/test/java/ru/javatrain/addressbook/ContactCreationTest.java
+++ address-book-web-tests/src/test/java/ru/javatrain/addressbook/ContactCreationTest.java
@@ -25,7 +25,7 @@ public class ContactCreationTest {
   public void testContactCreation() throws Exception {
     gotoContacts();
     createnewContact();
-    fillNewContact("Cont2", "Cont3", "Cont", "title", "COMPANY", "Novosibirsk", "nope","nope", "999999999", "tester", "999999991", "neveragain1@gmail.com", "neveragain2@gmail.com","neveragain2@gmail.com" );
+    fillNewContact(new ContactData("Cont2", "Cont3", "Cont", "title", "COMPANY", "Novosibirsk", "nope", "nope", "999999999", "tester", "999999991", "neveragain1@gmail.com", "neveragain2@gmail.com", "neveragain2@gmail.com"));
     submitnewContact();
     returntoContact();
   }
@@ -39,56 +39,56 @@ public class ContactCreationTest {
   }
 
   private void fillNewContact(String name, String patrname, String lastname, String nickname, String title, String company, String city, String home) {
-    fillNewContact(name, patrname, lastname, nickname, title, company, city, home, "999999999", "tester", "999999991", "neveragain1@gmail.com", "neveragain2@gmail.com", "neveragain2@gmail.com");
+    fillNewContact(new ContactData(name, patrname, lastname, nickname, title, company, city, home, "999999999", "tester", "999999991", "neveragain1@gmail.com", "neveragain2@gmail.com", "neveragain2@gmail.com"));
   }
 
-  private void fillNewContact(String name, String patrname, String lastname, String nickname, String title, String company, String city, String home, String mobphone, String position, String fax, String email1, String email2, String email3) {
+  private void fillNewContact(ContactData contactData) {
     driver.findElement(By.name("firstname")).click();
     driver.findElement(By.name("firstname")).click();
     driver.findElement(By.name("firstname")).clear();
-    driver.findElement(By.name("firstname")).sendKeys(name);
+    driver.findElement(By.name("firstname")).sendKeys(contactData.name());
     driver.findElement(By.name("middlename")).click();
     driver.findElement(By.name("middlename")).clear();
-    driver.findElement(By.name("middlename")).sendKeys(patrname);
+    driver.findElement(By.name("middlename")).sendKeys(contactData.patrname());
     driver.findElement(By.name("lastname")).click();
     driver.findElement(By.name("lastname")).clear();
-    driver.findElement(By.name("lastname")).sendKeys(lastname);
+    driver.findElement(By.name("lastname")).sendKeys(contactData.lastname());
     driver.findElement(By.name("nickname")).click();
     driver.findElement(By.name("nickname")).clear();
-    driver.findElement(By.name("nickname")).sendKeys(nickname);
+    driver.findElement(By.name("nickname")).sendKeys(contactData.nickname());
     driver.findElement(By.name("title")).click();
     driver.findElement(By.name("title")).clear();
-    driver.findElement(By.name("title")).sendKeys(title);
+    driver.findElement(By.name("title")).sendKeys(contactData.title());
     driver.findElement(By.name("company")).click();
     driver.findElement(By.name("company")).clear();
-    driver.findElement(By.name("company")).sendKeys(company);
+    driver.findElement(By.name("company")).sendKeys(contactData.company());
     driver.findElement(By.name("address")).click();
     driver.findElement(By.name("address")).clear();
-    driver.findElement(By.name("address")).sendKeys(city);
+    driver.findElement(By.name("address")).sendKeys(contactData.city());
     driver.findElement(By.name("theform")).click();
     driver.findElement(By.name("home")).click();
     driver.findElement(By.name("home")).clear();
-    driver.findElement(By.name("home")).sendKeys(home);
+    driver.findElement(By.name("home")).sendKeys(contactData.home());
     driver.findElement(By.name("mobile")).click();
     driver.findElement(By.name("mobile")).clear();
-    driver.findElement(By.name("mobile")).sendKeys(mobphone);
+    driver.findElement(By.name("mobile")).sendKeys(contactData.mobphone());
     driver.findElement(By.name("work")).click();
     driver.findElement(By.name("work")).clear();
-    driver.findElement(By.name("work")).sendKeys(position);
+    driver.findElement(By.name("work")).sendKeys(contactData.position());
     driver.findElement(By.name("fax")).click();
     driver.findElement(By.name("fax")).clear();
-    driver.findElement(By.name("fax")).sendKeys(fax);
+    driver.findElement(By.name("fax")).sendKeys(contactData.fax());
     driver.findElement(By.name("email")).click();
     driver.findElement(By.name("email")).clear();
-    driver.findElement(By.name("email")).sendKeys(email1);
+    driver.findElement(By.name("email")).sendKeys(contactData.email1());
     driver.findElement(By.name("theform")).click();
     driver.findElement(By.name("email")).click();
     driver.findElement(By.name("email2")).click();
     driver.findElement(By.name("email2")).clear();
-    driver.findElement(By.name("email2")).sendKeys(email2);
+    driver.findElement(By.name("email2")).sendKeys(contactData.email2());
     driver.findElement(By.name("email3")).click();
     driver.findElement(By.name("email3")).clear();
-    driver.findElement(By.name("email3")).sendKeys(email3);
+    driver.findElement(By.name("email3")).sendKeys(contactData.email3());
     driver.findElement(By.name("theform")).click();
   }
 
---
+++ address-book-web-tests/src/test/java/ru/javatrain/addressbook/ContactData.java
@@ -0,0 +1,6 @@
+package ru.javatrain.addressbook;
+
+public record ContactData(String name, String patrname, String lastname, String nickname, String title, String company,
+                          String city, String home, String mobphone, String position, String fax, String email1,
+                          String email2, String email3) {
+}
