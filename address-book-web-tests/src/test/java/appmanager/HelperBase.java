67eaba6c93abbbd71d0372b9c49e1e5315e857f8
PolinaRil <107141612+PolinaRil@users.noreply.github.com>
2022-07-05 19:46:08 +0300

    add HelperBase.java

---
+++ address-book-web-tests/src/test/java/appmanager/HelperBase.java
@@ -0,0 +1,26 @@
+package appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HelperBase {
+    public WebDriver driver;
+
+    public HelperBase(WebDriver driver) {
+        this.driver=driver;
+    }
+
+    protected void click(By locator) {
+        driver.findElement(locator).click();
+    }
+
+    protected void submit(By locator) {
+        driver.findElement(locator).submit();
+    }
+
+    protected void type(By locator, String text) {
+        driver.findElement(locator).click();
+        driver.findElement(locator).clear();
+        driver.findElement(locator).sendKeys(text);
+    }
+}
