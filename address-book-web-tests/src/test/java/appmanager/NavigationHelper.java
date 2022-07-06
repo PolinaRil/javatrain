493f450b56a315e47c89fc358d84f839c1658de1
PolinaRil <107141612+PolinaRil@users.noreply.github.com>
2022-07-06 18:24:02 +0300

    modify NavigationHelper.java

--- address-book-web-tests/src/test/java/appmanager/NavigationHelper.java
+++ address-book-web-tests/src/test/java/appmanager/NavigationHelper.java
@@ -3,15 +3,16 @@ package appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class NavigationHelper {
+public class NavigationHelper extends HelperBase{
     public WebDriver driver;
 
     public NavigationHelper(WebDriver driver) {
-       this.driver=driver;
+      // this.driver=driver;
+        super(driver);
 
     }
 
     public void gotoGroupPage() {
-    driver.findElement(By.linkText("groups")).click();
+    click(By.linkText("groups"));
   }
 }
