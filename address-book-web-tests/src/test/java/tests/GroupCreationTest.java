ae08c7f7d097dacf3a115d772bf9506377514891
PolinaRil <107141612+PolinaRil@users.noreply.github.com>
2022-08-24 18:06:57 +0300

    add parameter to GroupCreationTest

--- address-book-web-tests/src/test/java/tests/GroupCreationTest.java
+++ address-book-web-tests/src/test/java/tests/GroupCreationTest.java
@@ -6,23 +6,31 @@ import org.testng.annotations.*;
 import model.GroupData;
 import ru.javatrain.addressbook.TestBase;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTest extends TestBase {
-    @Test
-    public void testGroupCreation() throws Exception {
+
+    @DataProvider
+    public Iterator<Object[]> validGroups() {
+        List<Object[]> list = new ArrayList<Object[]>();
+        list.add(new Object[] {new GroupData().withName("test1").withHeader("header1").withFooter("footer1")});
+        list.add(new Object[] {new GroupData().withName("test2").withHeader("header2").withFooter("footer2")});
+        list.add(new Object[] {new GroupData().withName("test3").withHeader("header3").withFooter("footer3")});
+        return list.iterator();
+
+    }
+
+    @Test (dataProvider = "validGroups")
+    public void testGroupCreation(GroupData group) throws Exception {
         app.goTo().groupPage();
 
        Groups before = app.group().all();
 
         app.group().initGroupModification();
-        GroupData group = new GroupData().withName("test2");
         app.group().create(group);
         app.group().submitGroupPage();
         app.group().returntoGroupPage();
