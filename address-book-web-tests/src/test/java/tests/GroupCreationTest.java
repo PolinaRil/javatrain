aa4d52616f4ad7e9476fbd0c1979a47c1913bf36
PolinaRil <107141612+PolinaRil@users.noreply.github.com>
2022-08-26 18:21:36 +0300

    add xml to GroupCreationTest.javaTest

--- address-book-web-tests/src/test/java/tests/GroupCreationTest.java
+++ address-book-web-tests/src/test/java/tests/GroupCreationTest.java
@@ -1,42 +1,45 @@
 package tests;
 
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.security.AnyTypePermission;
 import model.Groups;
-import org.testng.Assert;
 import org.testng.annotations.*;
 import model.GroupData;
 import ru.javatrain.addressbook.TestBase;
 
 import java.io.*;
 import java.util.*;
-import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTest extends TestBase {
-
     @DataProvider
     public Iterator<Object[]> validGroups() throws IOException {
-        List<Object[]> list = new ArrayList<Object[]>();
-        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
+        String xml = "";
         String line = reader.readLine();
-        while(line != null)    {
-            String[] split = line.split(";");
-            list.add(new Object[] {new GroupData().
-                    withName(split[0]).
-                    withHeader(split[1]).
-                    withFooter(split[2])});
+
+        while (line != null) {
+            xml += line;
             line = reader.readLine();
         }
-        return list.iterator();
 
+        XStream xstream = new XStream();
+        xstream.addPermission(AnyTypePermission.ANY);
+        xstream.processAnnotations(GroupData.class);
+
+        List<GroupData> groups = (List<GroupData>) xstream.fromXML(xml);
+
+        return groups.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
     }
 
-    @Test (dataProvider = "validGroups")
+    @Test(dataProvider = "validGroups")
     public void testGroupCreation(GroupData group) throws Exception {
         app.goTo().groupPage();
 
-       Groups before = app.group().all();
+        Groups before = app.group().all();
 
         app.group().initGroupModification();
         app.group().create(group);
@@ -45,8 +48,7 @@ public class GroupCreationTest extends TestBase {
 
         Groups after = app.group().all();
 
-        assertThat(app.group().count(), equalTo(before.size() +1));
-
+        assertThat(app.group().count(), equalTo(before.size() + 1));
         assertThat(after, equalTo(before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
     }
 
@@ -64,9 +66,6 @@ public class GroupCreationTest extends TestBase {
         Groups after = app.group().all();
 
         assertThat(app.group().count(), equalTo(before.size()));
-
         assertThat(after, equalTo(before));
     }
-
-
 }
