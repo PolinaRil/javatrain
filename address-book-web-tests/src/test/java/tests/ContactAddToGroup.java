55628dd06eea1c664242aa748876f48c2aafa7a6
PolinaRil <107141612+PolinaRil@users.noreply.github.com>
2022-08-30 18:55:55 +0300

    add ContactAddToGroup.java with pain and suffer

---
+++ address-book-web-tests/src/test/java/tests/ContactAddToGroup.java
@@ -0,0 +1,38 @@
+package tests;
+
+import model.ContactData;
+import model.Contacts;
+import model.GroupData;
+import model.Groups;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import ru.javatrain.addressbook.TestBase;
+
+public class ContactAddToGroup extends TestBase {
+
+    @BeforeTest
+    public void ensurePreconditions() {
+        app.goTo().groupPage();
+
+        if (app.db().groups().size() == 0) {
+            app.group().initGroupModification();
+            app.group().create(new GroupData().withName("test1"));
+            app.group().submitGroupPage();
+            app.group().returntoGroupPage();
+        }
+    }
+
+    @Test
+    public void testContactAddToGroup() {
+        app.contact().returnToContact();
+        Groups groups = app.db().groups();
+        Contacts contacts = app.db().contacts();
+        ContactData modifiedContact = contacts.iterator().next();
+
+        System.out.println(modifiedContact.getGroups());
+
+        app.contact().inGroup(modifiedContact);
+        app.contact().returnToContact();
+    }
+
+}
