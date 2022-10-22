dc7b9e0bbdb8171d2bfe95870d04bb9da2eecece
PolinaRil <107141612+PolinaRil@users.noreply.github.com>
2022-10-22 10:44:45 +0300

    fix HttpSession.java, add LoginText.java

--- mantis-tests/src/test/java/ru/javatrain/mantis/appmanager/HttpSession.java
+++ mantis-tests/src/test/java/ru/javatrain/mantis/appmanager/HttpSession.java
@@ -1,20 +1,21 @@
 package ru.javatrain.mantis.appmanager;
 
-import org.apache.hc.client5.http.classic.methods.HttpGet;
-import org.apache.hc.client5.http.classic.methods.HttpPost;
-import org.apache.hc.client5.http.entity.UrlEncodedFormEntity;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
-import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.apache.hc.core5.http.NameValuePair;
-import org.apache.hc.core5.http.ParseException;
-import org.apache.hc.core5.http.io.entity.EntityUtils;
-import org.apache.hc.core5.http.message.BasicNameValuePair;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.ParseException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.client.LaxRedirectStrategy;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
 
 public class HttpSession {
     private CloseableHttpClient httpclient;
